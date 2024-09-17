3abc35843fc863106caa78bc3c5357ee930b315d
Thiboss <thibault.charbonnier@edu.devinci.fr>
2024-09-17 19:15:57 +0200

    Penible

---
+++ Python-Files/node.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, computed_field
+
+class Node(BaseModel):
+    
+    price : float
+    payoff : float = None
+    
+    next_up : 'Node' = None
+    next_mid : 'Node' = None
+    next_down : 'Node' = None
+    
+    prev_node : 'Node' = None
+    down_node : 'Node' = None
+    up_node : 'Node' = None
