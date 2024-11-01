80adae5a445065be0ed2cbe6bab0c5e827809e5f
Thiboss <thibault.charbonnier@edu.devinci.fr>
2024-11-01 14:06:44 +0100

    Last commit

--- PythonFiles/visualisation.py
+++ PythonFiles/visualisation.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
 import networkx as nx
 from PythonFiles.tree import Tree
 
@@ -11,19 +10,18 @@ def visualize_tree(tree : Tree, nb_steps):
     G = nx.DiGraph()  
     node_labels = {}
     pos = {}
-    # Ajout des noeuds et des arêtes par niveau
+
     def add_edges_by_level(node, level=0, parent=None, pos_y=0):
         if node is None:
             return
         node_label = f"{round(node.price, 2)}"
         node_id = f"{level}_{round(node.price, 2)}"
         node_labels[node_id] = node_label
-        # Ajout du noeud et de l'arête avec le parent
+
         G.add_node(node_id, level=level)
         if parent:
             G.add_edge(parent, node_id)
         pos[node_id] = (level, pos_y)
-        # Appel récursif pour les noeuds enfants
         add_edges_by_level(node.next_up, level+1, node_id, pos_y+1)
         add_edges_by_level(node.next_mid, level+1, node_id, pos_y)
         add_edges_by_level(node.next_down, level+1, node_id, pos_y-1)
@@ -31,7 +29,7 @@ def visualize_tree(tree : Tree, nb_steps):
     add_edges_by_level(tree.root_node, 0)
     fig = plt.figure(figsize=(15, 10))
     nx.draw(G, pos, labels=node_labels, with_labels=True, node_size=1000, node_color="lightblue", font_size=8, font_color="black", font_weight='bold', arrowsize=10)
-    plt.title("Arbre Trinomial - Visualisation 2D Optimisée (Prix uniquement)")
+    plt.title("Arbre Trinomial")
     return fig
 
 def plot_price_convergence(steps, prices_array, bs_price):
